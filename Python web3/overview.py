3d40837ecb0395ff6276da3194433c121828ebbe
Viacheslav <80632445+DemaPy@users.noreply.github.com>
2022-07-24 21:46:21 +0200

    Update overview.py

--- Python web3/overview.py
+++ Python web3/overview.py
@@ -1,6 +1,7 @@
 from eth_utils import to_wei
 from web3 import Web3
 from words import slovar
+import json
 import re
 
 def connectTo(provider="https://bsc-dataseed.binance.org/"):
@@ -11,6 +12,15 @@ def connectTo(provider="https://bsc-dataseed.binance.org/"):
         whatNext(connect)
     else:
         print(slovar[0])
+
+def checkLenInput(content):
+    while True:
+        content
+        if len(content) == 0:
+            print('Write correct address!')
+            continue
+        else:
+            break
 
 def whatNext(c):
     print(slovar[2])
@@ -26,6 +36,23 @@ def whatNext(c):
     elif choose == '4':
         key = input(slovar[9]).strip()
         sendTransaction(c,key)
+    elif choose == '5':
+        while True:
+            first = input('Первый токен: ').strip()
+            if len(first) == 0:
+                print('Write correct address!')
+                continue
+            else:
+                break
+        # checkLenInput(first)
+        while True:
+            second = input('Второй токен: ').strip()
+            if len(second) == 0:
+                print('Write correct address!')
+                continue
+            else:
+                break
+        getPair(c,first,second)
     else:
         print(slovar[4])
         
@@ -56,6 +83,11 @@ def sendTransaction(c,key):
     signed_txn = c.eth.account.signTransaction(tx, key)
     txn_hash = c.eth.sendRawTransaction(signed_txn.rawTransaction)
     print(txn_hash)
+
+def getPair(c, first, second):
+    factory = '0xca143ce32fe78f1f7019d7d551a6402fc5350c73'
+    contract = c.eth.contract(address=Web3.toChecksumAddress(factory), abi=json.loads('[{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"getPair","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]'))
+    print(f'Contract liqudity is: {contract.functions.getPair(first, second).call()}')
 
 if __name__ == '__main__':
     q = input(slovar[7])
